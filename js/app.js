df60209d27aa2ca8b2eace18048289dbdce05e70
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-03-13 16:57:58 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+
+
 var revertIt = false;
+
+
+
+$(document).ready(function(){
+  $.getJSON("../store/store.json", function(data){
+      console.log(data);
+  })
+})
 
 $(function () {
   $(".drag-obj").draggable({ revert: revertIt });
+  
 
   $("#drop-place").droppable({
     drop: function (event, ui) {
