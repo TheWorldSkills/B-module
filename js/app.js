825f67f124ce3213f31ae8c00d6bcf6391449755
Goolgae <sunwoo0706@icloud.com>
2021-03-13 16:04:47 +0900

    Implement feature/droppable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+var revertIt = false;
+
 $(function () {
-  $(".drag-obj").draggable({ revert: true });
+  $(".drag-obj").draggable({ revert: revertIt });
 
   $("#drop-place").droppable({
     drop: function (event, ui) {
-      $(this).addClass("ui-state-highlight").find("p").html("Dropped!");
+      $(this).addClass("ui-state-highlight");
     },
   });
 });
