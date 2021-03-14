3f735ad1968cda056772c8e23cee7719654cf99c
Goolgae <sunwoo0706@icloud.com>
2021-03-14 14:01:24 +0900

    Update doc linking json

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
+var is_revert = true;
 
-
-var revertIt = false;
-
-
-
-$(document).ready(function(){
-  $.getJSON("../store/store.json", function(data){
-      console.log(data);
-  })
-})
+$(document).ready(function () {
+  $.getJSON("../store/store.json", function (data, textStatus) {
+    console.log(data, textStatus);
+    for (var i = 0; i < data.length; i++) {
+      $("#drag-wrap").append(
+        `<div class="grid-item drag-obj">${data[i].product_name}<br />${data[i].brand}</div>`
+      );
+      console.log(data[i]);
+      console.log(data[i].id);
+      console.log(data[i].photo);
+      console.log(data[i].brand);
+      console.log(data[i].product_name);
+      console.log(data[i].price);
+    }
+  });
+});
 
 $(function () {
-  $(".drag-obj").draggable({ revert: revertIt });
-  
+  $(".drag-obj").draggable({ revert: is_revert });
 
   $("#drop-place").droppable({
     drop: function (event, ui) {
-      $(this).addClass("ui-state-highlight");
+      is_revert = false;
     },
   });
 });
