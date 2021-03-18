007e32040edd5372ee655a4c11014ec2a19895dd
Goolgae <sunwoo0706@icloud.com>
2021-03-18 17:27:03 +0900

    Add publish del-box and list-box

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var isRevert = true;
 
 $(document).ready(function () {
   $.getJSON("../store/store.json", function (data, textStatus) {
-    console.log(data, textStatus);
     for (var i = 0; i < data.length; i++) {
       $("#drag-wrap").append(
         `<div class="grid-item drag-obj"><img src="../store/상품사진/${data[i].photo}" /><br />상품명 : ${data[i].product_name}<br />브랜드 : ${data[i].brand}<br />가격 : ${data[i].price}원</div>`
